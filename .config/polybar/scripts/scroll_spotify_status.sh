0496466a5706a6b174d50bb1c55eb852f54fe1f2
Mykola V <nikola@rootnix.net>
2024-02-09 14:49:27 +0100

    poly spoti scripts

---
+++ .config/polybar/scripts/scroll_spotify_status.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+zscroll -l 15 \
+        --delay 0.25 \
+        --scroll-padding " | " \
+        --match-command "`dirname $0`/get_spotify_status.sh --status" \
+        --match-text "Playing" "--scroll 1" \
+        --match-text "Paused" "--scroll 0" \
+        --update-check true "`dirname $0`/get_spotify_status.sh" &
+
+wait
+
